29d963d88f7a46bca9da110bad8af8cb95fce3c7
Daniels Nagornuks <20106595@mail.wit.ie>
2025-12-09 16:39:19 +0000

    Added Cors

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,22 +1,24 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import cors from 'cors';
 import './db';
 
 dotenv.config();
 
 const errHandler = (err, req, res) => {
-  /* if the error in development then send stack trace to display whole error,
-  if it's in production then just send error message  */
-  if(process.env.NODE_ENV === 'production') {
-    return res.status(500).send(`Something went wrong!`);
-  }
-  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+    /* if the error in development then send stack trace to display whole error,
+    if it's in production then just send error message  */
+    if (process.env.NODE_ENV === 'production') {
+        return res.status(500).send(`Something went wrong!`);
+    }
+    res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
 };
 
 
 const app = express();
 const port = process.env.PORT;
+app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 app.use(errHandler);
